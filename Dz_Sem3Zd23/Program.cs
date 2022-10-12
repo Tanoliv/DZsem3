679a790f3f003f0b6de9ad1ed260e12d3d1a808d
Oleg <tmr777@mail.ru>
2022-10-12 11:12:24 +0300

    Таблица кубов

---
+++ Dz_Sem3Zd23/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число:"); 
+int N = int.Parse(Console.ReadLine()!);
+int resalt = 1;
+int cub;
+
+if(N < 1)
+{
+    Console.WriteLine("Ввели некорректное число. Введите число равное или больше 1.");
+}
+
+else
+{
+     while (resalt < N)
+    {
+        cub = resalt * resalt * resalt;
+        Console.Write($"{cub},");
+        resalt++;
+    }
+    Console.Write($"{Math.Pow(N, 3)}");
+}
+
